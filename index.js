f4bffb358fb66d841d5e3f5e6d70c2af7f666b91
claudia <claudialaffargue@outlook.com>
2025-02-26 00:10:09 +0100

    Affichage dynamique de la galerie

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-/**
- * Récupération des travaux puis affichage dynamique
- */
+/**Récupération des travaux puis affichage dynamique */
 async function afficherTravaux(){
     /**Récupération dans constante travaux*/
     const reponse = await fetch("http://localhost:5678/api/works");
     const travaux = await reponse.json();
     /**Création des nouveaux éléments et rattachement au DOM*/
-    const gallerie = document.createElement("div");
+    const galerie = document.createElement("div");
     for(let i=0; i<travaux.length;i++){
-        gallerie.innerHTML+=`<figure><img src="${travaux[i].imageUrl}" alt="${travaux[i].title}"><figcaption>${travaux[i].title}</figcaption></figure>`
+        galerie.innerHTML+=`<figure><img src="${travaux[i].imageUrl}" alt="${travaux[i].title}"><figcaption>${travaux[i].title}</figcaption></figure>`
     }
     const portfolio = document.getElementById("portfolio");
-    portfolio.appendChild(gallerie);  
+    portfolio.appendChild(galerie);
     /**Ajout de la classe pour mise en page css de la div */
-    gallerie.classList.add("gallery");
+    galerie.classList.add("gallery");
 }
 afficherTravaux();
